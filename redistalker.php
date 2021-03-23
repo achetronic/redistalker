dc10b258fd34cdafea77ecbb69a023527492bfba
Alby Hernández <donfumero@gmail.com>
2021-03-23 02:01:07 +0000

    New InitController for Redis created to switch between modes (monitor or pubsub)

--- redistalker.php
+++ redistalker.php
@@ -3,8 +3,18 @@
 // Import all classes from Composer packages
 require_once __DIR__ . '/kernel.php';
 
-use App\Controllers\Redis\MonitorController;
+// use App\Controllers\Redis\MonitorController;
+// use App\Controllers\Redis\PubsubController;
 
-// Execute the main flow of the project
-$redis = new MonitorController;
-$redis->main();
+use App\Controllers\Redis\InitController;
+
+// Execute the main flow of the monitor
+// $redis = new MonitorController;
+// $redis->main();
+
+// Execute the main flow of the pubsub
+// $redis = new PubsubController;
+// $redis->main();
+
+// Switch between the Redis consumer loop mode
+$redis = (new InitController('pubsub'))->run();
